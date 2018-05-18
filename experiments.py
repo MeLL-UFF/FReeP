066003787552ce350129b11ecbbe36934cf445f7
DanielJunior <danieljunior@id.uff.br>
2018-05-18 19:57:52 -0300

    Alterando execução dos experimentos

--- experiments.py
+++ experiments.py
@@ -19,8 +19,12 @@ kf = KFold(n_splits=5)
 neighbors = [3, 5, 7]
 
 with open('results.txt', 'w') as f:
-    for train_index, test_index in kf.split(data):
-        for neighbor in neighbors:
+    for neighbor in neighbors:
+
+        f.write('K = %d\n' % neighbor)
+        print('K = %d' % neighbor)
+
+        for train_index, test_index in kf.split(data):
             knn_recommender = KNNFeatureRecommender(data.iloc[train_index], neighbors=neighbor)
             rank_recommender = RankFeatureRecommender(data.iloc[train_index], neighbors=neighbor)
             knn_true_label = []
@@ -29,6 +33,10 @@ with open('results.txt', 'w') as f:
             rank_pred = []
             start = time.time()
             random_feature = random.choice(list(data.columns))
+
+            f.write('FEATURE = %s\n\n' % random_feature)
+            print("FEATURE %s\n" % random_feature)
+
             for idx in test_index:
                 preferences = data.iloc[idx].to_dict()
                 true_value = preferences[random_feature]
@@ -43,47 +51,48 @@ with open('results.txt', 'w') as f:
                     rank_pred.append(rank_recomendation)
             end = time.time()
             elapsed = end - start
-            print("Tempo (segundos): ", elapsed)
 
-            f.write('FEATURE: ' + random_feature + "\n")
-            f.write('%s \t %s \t %s \t %s \t %s\n' % ("MODELO", "K", "ACURACIA", "PRECISAO", "RECALL"))
+            f.write('%s \t %s \t %s \t %s\n' % ("MODELO", "ACURACIA", "PRECISAO", "RECALL"))
+            print('%s \t %s \t %s \t %s' % ("MODELO", "ACURACIA", "PRECISAO", "RECALL"))
 
-            print("************KNN***********")
             knn_pred_label = [str(pred[0]) for pred in knn_pred]
             knn_true_label = [str(elem) for elem in knn_true_label]
             knn_accuracy = accuracy_score(knn_true_label, knn_pred_label)
             knn_precision = precision_score(knn_true_label, knn_pred_label, average='micro')
             knn_recall = recall_score(knn_true_label, knn_pred_label, average='micro')
-            print(knn_true_label)
-            print(knn_pred_label)
-            print("Acurácia: ", knn_accuracy)
-            print("Precisão: ", knn_precision)
-            print("Recall: ", knn_recall)
 
-            f.write('%s \t %d \t %f \t %f \t %f\n' % ('KNN', neighbor, knn_accuracy, knn_precision, knn_recall))
+            f.write('%s  \t %f \t %f \t %f\n' % ('KNN', knn_accuracy, knn_precision, knn_recall))
             f.write('TRUE_LABEL : ' + ', '.join([str(elem) for elem in knn_true_label]))
             f.write("\n")
             f.write('PRED_LABEL : ' + ', '.join([str(elem) for elem in knn_pred_label]))
-            f.write("\n")
+            f.write("\n\n")
 
-            f.write("\n")
+            print('%s  \t %f \t %f \t %f' % ('KNN', knn_accuracy, knn_precision, knn_recall))
+            print('TRUE_LABEL : ' + ', '.join([str(elem) for elem in knn_true_label]))
+            print('PRED_LABEL : ' + ', '.join([str(elem) for elem in knn_pred_label]))
+            print("\n")
 
-            print("************RANK***********")
             rank_pred_label = [str(pred[0]) for pred in rank_pred]
             rank_true_label = [str(elem) for elem in rank_true_label]
-            print(rank_true_label)
-            print(rank_pred_label)
             rank_accuracy = accuracy_score(rank_true_label, rank_pred_label)
             rank_precision = precision_score(rank_true_label, rank_pred_label, average='micro')
             rank_recall = recall_score(rank_true_label, rank_pred_label, average='micro')
-            print("Acurácia: ", accuracy_score(rank_true_label, rank_pred_label))
-            print("Precisão: ", precision_score(rank_true_label, rank_pred_label, average='micro'))
-            print("Recall: ", recall_score(rank_true_label, rank_pred_label, average='micro'))
 
             f.write('%s \t %d \t %f \t %f \t %f\n' % ('RANK', neighbor, rank_accuracy, rank_precision, rank_recall))
             f.write('TRUE_LABEL : ' + ', '.join([str(elem) for elem in rank_true_label]))
             f.write("\n")
             f.write('PRED_LABEL : ' + ', '.join([str(elem) for elem in rank_pred_label]))
-            f.write("\n")
+            f.write("\n\n")
             f.write('%s %f\n' % ("TEMPO: ", elapsed))
-        f.write("###################")
+            f.write("\n\n")
+
+            print('%s \t %d \t %f \t %f \t %f\n' % ('RANK', neighbor, rank_accuracy, rank_precision, rank_recall))
+            print('TRUE_LABEL : ' + ', '.join([str(elem) for elem in rank_true_label]))
+            print("\n")
+            print('PRED_LABEL : ' + ', '.join([str(elem) for elem in rank_pred_label]))
+            print("\n\n")
+            print('%s %f\n' % ("TEMPO: ", elapsed))
+            print("\n\n")
+
+        f.write("####################################\n")
+        print("######################################")
