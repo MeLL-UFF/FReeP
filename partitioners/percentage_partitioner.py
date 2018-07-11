0ab8cd47f43cabe6d1edd734f154d219117dfad5
DanielJunior <danieljunior@id.uff.br>
2018-07-10 21:12:48 -0300

    REFS #10 : Adicionando estratégia de seleção de features usando L2-Norm

--- partitioners/percentage_partitioner.py
+++ partitioners/percentage_partitioner.py
@@ -1,7 +1,5 @@
 from partitioners.partitioner import Partitioner
 from sklearn.feature_selection import SelectPercentile
-from functools import reduce
-
 
 class PercentagePartitioner(Partitioner):
     def __init__(self, percentile=50):
@@ -10,6 +8,6 @@ class PercentagePartitioner(Partitioner):
 
     def partition(self, X, y, preferences_columns):
         feature_selection = SelectPercentile(percentile=self.percentile)
-        feature_selection.fit(X, y)
-        columns = X.columns[feature_selection.get_support()].values
-        return super(PercentagePartitioner, self).powerset(preferences_columns)
+        feature_selection.fit(X[preferences_columns], y)
+        columns = X[preferences_columns].columns[feature_selection.get_support()].values
+        return super(PercentagePartitioner, self).powerset(columns)
