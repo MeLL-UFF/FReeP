4f50df89ca90ff5c1628a03055e7ac2804b9970d
DanielJunior <danieljunior@id.uff.br>
2018-07-31 21:47:04 -0300

    REFS #17 : Criação de script de experimentos para atributos categoricos e numericos

--- experiments_works.py
+++ experiments_works.py
@@ -10,6 +10,8 @@ from sklearn.neural_network import MLPClassifier
 from recommenders.regressor_feature_recommender import RegressorFeatureRecommender
 from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
 from partitioners.full_partitioner import FullPartitioner
 from partitioners.percentage_partitioner import PercentagePartitioner
 from partitioners.pca_partitioner import PCAPartitioner
@@ -18,6 +20,7 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
+from sklearn.metrics import mean_squared_error
 
 
 def partitioner_name(partitioner):
@@ -43,6 +46,22 @@ def classifier_name(classifier):
         else:
             return 'KNN 7'
 
+def regressor_name(regressor):
+    if type(regressor) is SVR:
+        return 'SVR'
+    elif type(regressor) is MLPRegressor:
+        return 'Multi Layer Perceptron'
+    elif type(regressor) is LinearRegression:
+        return 'Linear Regression'
+    else:   
+        # KNR
+        if regressor.n_neighbors == 3:
+            return 'KNR 3'
+        elif regressor.n_neighbors == 5:
+            return 'KNR 5'
+        else:
+            return 'KNR 7'
+
 
 data = pd.read_csv('data.csv', float_precision='round_trip')
 # apenas os dados de sucesso, sem a coluna de erro
@@ -51,7 +70,6 @@ data = data[~data['erro']].copy().drop('erro', axis=1).reset_index(drop=True)
 kf = KFold(n_splits=5)
 
 categorical_features = ['model1', 'model2']
-numerical_features = ['num_aligns', 'length', 'prob1', 'prob2']
 
 classifiers = [KNeighborsClassifier(n_neighbors=3),
                KNeighborsClassifier(n_neighbors=5),
@@ -65,13 +83,16 @@ partitioners = [
     PercentagePartitioner()
 ]
 
+print("CLASSIFICAÇÃO")
+
 with open('results/categorical_results' + time.strftime('%a, %d %b %Y %H:%M:%S ') + '.csv', 'w') as f:
     writer = csv.writer(f, delimiter=';')
     writer.writerow(
-                    ['FEATURE', 'CLASSIFIER', 'PARTITIONER', 'ACCURACY', 'TIME'])
+        ['FEATURE', 'CLASSIFIER', 'PARTITIONER', 'ACCURACY', 'TIME'])
     for feature in categorical_features:
         print("FEATURE %s" % feature)
         for classifier in classifiers:
+            print("CLASSIFIER %s" % classifier)
             for partitioner in partitioners:
                 start = time.time()
                 for train_index, test_index in kf.split(data):
@@ -96,12 +117,68 @@ with open('results/categorical_results' + time.strftime('%a, %d %b %Y %H:%M:%S '
                                                                    classifier=classifier)
                         recomendation = recommender.recommend(
                             feature, resp)
-                        true_label.append(true_value)
-                        pred_label.append(recomendation[0])
+                        if recomendation != None: 
+                            true_label.append(true_value)
+                            pred_label.append(recomendation[0])
                 end = time.time()
                 elapsed = end - start
                 accuracy = accuracy_score(true_label, pred_label)
                 clf_name = classifier_name(classifier)
                 part_name = partitioner_name(partitioner)
-                writer.writerow(
-                    [feature, clf_name, part_name, accuracy, elapsed])
+                row = [feature, clf_name, part_name, accuracy, elapsed]
+                print(row)
+                writer.writerow(row)
+
+print("############################")
+print("REGRESSÃO")
+
+numerical_features = ['num_aligns', 'length', 'prob1', 'prob2']
+regressors = [
+    LinearRegression(),
+    KNeighborsRegressor(n_neighbors=3),
+    KNeighborsRegressor(n_neighbors=5),
+    KNeighborsRegressor(n_neighbors=7),
+    SVR(),
+    MLPRegressor(solver='sgd', hidden_layer_sizes=(6,), random_state=1)]
+
+with open('results/numerical_results' + time.strftime('%a, %d %b %Y %H:%M:%S ') + '.csv', 'w') as f:
+    writer = csv.writer(f, delimiter=';')
+    writer.writerow(
+        ['FEATURE', 'REGRESSOR', 'PARTITIONER', 'ACCURACY', 'TIME'])
+    for feature in numerical_features:
+        print("FEATURE %s" % feature)
+        for regressor in regressors:
+            for partitioner in partitioners:
+                start = time.time()
+                for train_index, test_index in kf.split(data):
+                    train_data = data.iloc[train_index]
+                    y = data[feature]
+                    X = data.drop(feature, axis=1)
+                    true_label = []
+                    pred_label = []
+
+                    for idx in test_index:
+                        preferences = data.iloc[idx].to_dict()
+                        true_value = preferences[feature]
+                        del preferences[feature]
+                        resp = []
+                        for key, value in preferences.items():
+                            if type(value) is str:
+                                resp.append(str(key) + " == " +
+                                            "'"+str(value)+"'")
+                            else:
+                                resp.append(str(key) + ' == ' + str(value))
+                        recommender = RegressorFeatureRecommender(X, y, partitioner,
+                                                                   regressor=regressor)
+                        recomendation = recommender.recommend(
+                            feature, resp)
+                        true_label.append(true_value)
+                        pred_label.append(recomendation[0])
+                end = time.time()
+                elapsed = end - start
+                mse = mean_squared_error(true_label, pred_label)
+                regr_name = regressor_name(regressor)
+                part_name = partitioner_name(partitioner)
+                row = [feature, regr_name, part_name, mse, elapsed]
+                print(row)
+                writer.writerow(row)
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from recommenders.classifier_feature_recommender import ClassifierFeatureRecomme
 from recommenders.regressor_feature_recommender import RegressorFeatureRecommender
 from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
 from partitioners.full_partitioner import FullPartitioner
 from partitioners.percentage_partitioner import PercentagePartitioner
 from partitioners.l2_norm_partitioner import L2NormPartitioner
@@ -44,46 +46,56 @@ data = data[~data['erro']].copy().drop('erro', axis=1).reset_index(drop=True)
 
 # print("\n####################################\n")
 
-feature = 'model2'
-preferences = [
-    "( model1 == 'WAG+G' ) | ( model1 == 'WAG+I+F' )",
-    "prob1 >= 1500",
-    "length > 350"
-    ]
-y = data[feature]
-X = data.drop(feature, axis=1)
+# feature = 'model2'
+# preferences = [
+#     "( model1 == 'WAG+G' ) | ( model1 == 'WAG+I+F' )",
+#     "prob1 >= 1500",
+#     "length > 350"
+#     ]
+# y = data[feature]
+# X = data.drop(feature, axis=1)
 
-print("Preferências: ", preferences)
-recommender = RankFeatureRecommender(X, y, PCAPartitioner(), classifier=KNeighborsClassifier(n_neighbors=3))
-recomendation = recommender.recommend(feature, preferences)
-print("Recomendação por RANK para", feature, 'é', recomendation)
+# print("Preferências: ", preferences)
+# recommender = RankFeatureRecommender(X, y, PCAPartitioner(), classifier=KNeighborsClassifier(n_neighbors=3))
+# recomendation = recommender.recommend(feature, preferences)
+# print("Recomendação por RANK para", feature, 'é', recomendation)
 
 # print("Preferências: ", preferences)
 # recommender = ClassifierFeatureRecommender(X, y, PCAPartitioner(), classifier=KNeighborsClassifier(n_neighbors=3))
 # recomendation = recommender.recommend(feature, preferences)
 # print("Recomendação por KNN para", feature, 'é', recomendation)
 
-recommender = ClassifierFeatureRecommender(X, y, PCAPartitioner(), classifier=SVC(probability=True))
-recomendation = recommender.recommend(feature, preferences)
-print("Recomendação por SVM para", feature, 'é', recomendation)
+# recommender = ClassifierFeatureRecommender(X, y, PCAPartitioner(), classifier=SVC(probability=True))
+# recomendation = recommender.recommend(feature, preferences)
+# print("Recomendação por SVM para", feature, 'é', recomendation)
 
 # recommender = ClassifierFeatureRecommender(X, y, PercentagePartitioner(), classifier=SVC(probability=True))
 # recomendation = recommender.recommend(feature, preferences)
 # print("Recomendação por SVM para", feature, 'é', recomendation)
 
-print("\n####################################\n")
+# print("\n####################################\n")
 
 feature = 'prob1'
 preferences = [
     "( model1 == 'WAG+G' ) | ( model1 == 'WAG+I+F' )",
     "num_aligns == 10"
-    ]
+]
 
 y = data[feature]
 X = data.drop(feature, axis=1)
 
-# print("Preferências: ", preferences)
-# recommender = RegressorFeatureRecommender(X, y, PercentagePartitioner())
-# recomendation = recommender.recommend(feature, preferences)
-# print("Recomendação por KNR para", feature, 'é', recomendation)
+print("Preferências: ", preferences)
 
+regressors = [
+    MLPRegressor(solver='sgd', random_state=1),
+    LinearRegression(),
+    KNeighborsRegressor(n_neighbors=3),
+    KNeighborsRegressor(n_neighbors=5),
+    KNeighborsRegressor(n_neighbors=7),
+    SVR()]
+for regressor in regressors:
+    recommender = RegressorFeatureRecommender(X, y, PCAPartitioner(),
+                                              regressor=regressor)
+    recomendation = recommender.recommend(
+        feature, preferences)
+    print("Recomendação para", feature, 'é', recomendation)
