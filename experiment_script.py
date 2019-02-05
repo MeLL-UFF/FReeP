cdf108e61944c8f3d52c0653e890547685f894b0
DanielJunior <danieljunior@id.uff.br>
2019-02-05 16:19:00 -0200

    Movendo os resultados de experimentos do lab para pasta separada.

--- experiment_script.py
+++ experiment_script.py
@@ -75,9 +75,9 @@ class ExperimentScript():
 
     def partitioners(self):
         return [
-            FullPartitioner(),
-            # PCAPartitioner(),
-            # PercentagePartitioner()
+            PCAPartitioner(),
+            PercentagePartitioner(),
+            FullPartitioner()
         ]
 
     def regressors(self):
