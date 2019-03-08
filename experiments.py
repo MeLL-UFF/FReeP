6d6a348dec232ba7a75a0c5e1090406ea7880099
DanielJunior <danieljunior@id.uff.br>
2019-03-08 17:56:59 -0300

    Refatorando codigo de experimento do sbbd2018 para ter compatibilidade com os outros resultados

--- experiments.py
+++ experiments.py
@@ -1,5 +1,6 @@
 import time
 import json
+import csv
 import random
 import pandas as pd
 from custom_knn_feature_recommender import CustomKNNFeatureRecommender
@@ -10,94 +11,80 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
+import itertools
+import importlib
+import multiprocessing as mp
 
-data = pd.read_csv('data.csv', float_precision='round_trip')
-# apenas os dados de sucesso, sem a coluna de erro
-data = data[~data['erro']].copy().drop('erro', axis=1).reset_index(drop=True)
 
-kf = KFold(n_splits=5)
+def neighbors():
+    return [3, 5, 7]
 
-neighbors = [3, 5, 7]
-features = list(data.columns)
+def execution(params):
+    neighbor = params[0]
+    kf = params[1]
+    data = params[2]
+    feature = params[3]
+    _class = params[4]
+    submodule = ""
+    if _class == "KNNFeatureRecommender":
+        submodule = "knn_feature_recommender"
+    else:
+        submodule = "rank_feature_recommender"
 
-with open('results.txt' + time.strftime('%a, %d %b %Y %H:%M:%S '), 'w') as f:
     start = time.time()
-    for feature in features:
-
-        f.write('FEATURE = %s\n\n' % feature)
-        print("FEATURE %s" % feature)
-
-        for neighbor in neighbors:
-
-            f.write('K = %d\n' % neighbor)
-            print('K = %d' % neighbor)
-
-            for train_index, test_index in kf.split(data):
-                knn_recommender = KNNFeatureRecommender(data.iloc[train_index], neighbors=neighbor)
-                rank_recommender = RankFeatureRecommender(data.iloc[train_index], neighbors=neighbor)
-                knn_true_label = []
-                knn_pred = []
-                rank_true_label = []
-                rank_pred = []
-
-                for idx in test_index:
-                    preferences = data.iloc[idx].to_dict()
-                    true_value = preferences[feature]
-                    del preferences[feature]
-
-                    f.write("PREFERÊNCIAS: ")
-                    f.write(json.dumps(preferences))
-                    f.write("\n")
-
-                    knn_recomendation = knn_recommender.recommend(feature, preferences)
-                    rank_recomendation = rank_recommender.recommend(feature, preferences)
-                    if knn_recomendation:
-                        knn_true_label.append(true_value)
-                        knn_pred.append(knn_recomendation)
-                    if rank_recomendation:
-                        rank_true_label.append(true_value)
-                        rank_pred.append(rank_recomendation)
-
-                f.write('%s \t %s \t %s \t %s\n' % ("MODELO", "ACURACIA", "PRECISAO", "RECALL"))
-                print('%s \t %s \t %s \t %s' % ("MODELO", "ACURACIA", "PRECISAO", "RECALL"))
-
-                knn_pred_label = [str(pred[0]) for pred in knn_pred]
-                knn_true_label = [str(elem) for elem in knn_true_label]
-                knn_accuracy = accuracy_score(knn_true_label, knn_pred_label)
-                knn_precision = precision_score(knn_true_label, knn_pred_label, average='micro')
-                knn_recall = recall_score(knn_true_label, knn_pred_label, average='micro')
-
-                f.write('%s  \t %f \t %f \t %f\n' % ('KNN', knn_accuracy, knn_precision, knn_recall))
-                f.write('TRUE_LABEL : ' + ', '.join([str(elem) for elem in knn_true_label]))
-                f.write("\n")
-                f.write('PRED_LABEL : ' + ', '.join([str(elem) for elem in knn_pred_label]))
-                f.write("\n\n")
-
-                print('%s  \t %f \t %f \t %f' % ('KNN', knn_accuracy, knn_precision, knn_recall))
-                print('TRUE_LABEL : ' + ', '.join([str(elem) for elem in knn_true_label]))
-                print('PRED_LABEL : ' + ', '.join([str(elem) for elem in knn_pred_label]))
-                print("\n")
-
-                rank_pred_label = [str(pred[0]) for pred in rank_pred]
-                rank_true_label = [str(elem) for elem in rank_true_label]
-                rank_accuracy = accuracy_score(rank_true_label, rank_pred_label)
-                rank_precision = precision_score(rank_true_label, rank_pred_label, average='micro')
-                rank_recall = recall_score(rank_true_label, rank_pred_label, average='micro')
-
-                f.write('%s \t %d \t %f \t %f \t %f\n' % ('RANK', neighbor, rank_accuracy, rank_precision, rank_recall))
-                f.write('TRUE_LABEL : ' + ', '.join([str(elem) for elem in rank_true_label]))
-                f.write("\n")
-                f.write('PRED_LABEL : ' + ', '.join([str(elem) for elem in rank_pred_label]))
-                f.write("\n\n")
-
-                print('%s \t %d \t %f \t %f \t %f\n' % ('RANK', neighbor, rank_accuracy, rank_precision, rank_recall))
-                print('TRUE_LABEL : ' + ', '.join([str(elem) for elem in rank_true_label]))
-                print("\n")
-                print('PRED_LABEL : ' + ', '.join([str(elem) for elem in rank_pred_label]))
-                print("\n\n")
-            f.write("####################################\n")
-            print("######################################")
+    true_label = []
+    pred_label = []
+    # Load "module.submodule.MyClass"
+    MyClass = getattr(importlib.import_module(submodule), _class)
+    for train_index, test_index in kf.split(data):
+        recommender = MyClass(
+            data.iloc[train_index], neighbors=neighbor)
+        for idx in test_index:
+            preferences = data.iloc[idx].to_dict()
+            true_value = preferences[feature]
+            del preferences[feature]
+            recomendation = recommender.recommend(
+                feature, preferences)
+            if recomendation:
+                true_label.append(true_value)
+                pred_label.append(recomendation)
     end = time.time()
     elapsed = end - start
-    f.write('%s %f\n' % ("TEMPO TOTAL: ", elapsed))
-    f.write("\n\n")
+    pred_label = [str(pred[0]) for pred in pred_label]
+    true_label = [str(elem) for elem in true_label]
+    precision = precision_score(
+        true_label, pred_label, average='weighted')
+    recall = recall_score(
+        true_label, pred_label, average='weighted')
+    return [feature, _class, precision, recall, elapsed]
+
+def run(data, path):
+    # data = pd.read_csv('data.csv', float_precision='round_trip')
+    # # apenas os dados de sucesso, sem a coluna de erro
+    # data = data[~data['erro']].copy().drop('erro', axis=1).reset_index(drop=True)
+
+    features = list(data.columns)
+    kf = KFold(n_splits=5)
+    with open(path, 'w') as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerow(
+            ['FEATURE', 'MODEL', 'PRECISION', 'RECALL', 'TIME'])
+        paramlist = list(itertools.product(neighbors(), [kf], [data],
+                                           features, ["KNNFeatureRecommender", "RankFeatureRecommender"]))
+        pool = mp.Pool(1)
+        res = pool.map(execution, paramlist)
+        for row in res:
+            writer.writerow(row)
+
+sciphy = pd.read_csv('sciphy.csv', float_precision='round_trip')
+sciphy = sciphy[~sciphy['erro']].copy().drop('erro', axis=1).reset_index(drop=True)
+path = 'results/sciphy/original'+ \
+    time.strftime('%a, %d %b %Y %H:%M:%S ') + '.csv'
+run(sciphy, path)
+
+montage = pd.read_csv('montage.csv', float_precision='round_trip')
+columns = ['cntr', 'ra', 'dec', 'cra', 'cdec', 'crval1', 'crval2', 'crota2']
+montage = montage[columns]
+path = 'results/montage/original'+ \
+    time.strftime('%a, %d %b %Y %H:%M:%S ') + '.csv'
+run(montage, path)
